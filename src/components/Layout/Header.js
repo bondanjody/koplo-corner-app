b24899702f694cc9d1379abbd5eaeedbb1f18add
bondanjody <bondanjodys@gmail.com>
2023-03-03 12:47:16 +0700

    Memberi efek hover pada sideMenu navbar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
         <header className={classes.header}>
             <h1>Koplo Corner</h1>
             <div className={classes.sideMenu}>
-                <a href="#">About</a>
-                <a href="#">Contribute</a>
+
+                    <a href="#">About</a>
+                    <a href="#">Contribute</a>
             </div>
         </header>
     </Fragment>
