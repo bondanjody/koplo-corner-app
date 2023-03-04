cf906a0b675dcbdca7a4a6805a6c9898458a6fae
bondanjody <bondanjodys@gmail.com>
2023-03-04 23:56:19 +0700

    Menyempurnakan elemen trending. Memperbaiki tombol close yang out of position

--- src/components/Layout/ContentContainer.js
+++ src/components/Layout/ContentContainer.js
@@ -11,23 +11,25 @@ const Backdrop = () => {
 
 const Content = props => {
     return <div className={classes.modal}>
-        <div className={classes['image-container']}>
-            <img src={props.lyricsData.image} alt={`${props.lyricsData}.png`} />
-        </div>
-        <div className={classes['song-container']}>
-            <div className={classes['close-lyrics']} onClick={props.onHide}>X</div>
-            <div className={classes['song-title']}>
-                <p className={classes['title']}>{props.lyricsData.title}</p>
-                <p className={classes['title-translation']}>{props.lyricsData.titleTranslation}</p>
+        <div className={classes.container}>
+            <div className={classes['image-container']}>
+                <img src={props.lyricsData.image} alt={`${props.lyricsData}.png`} />
             </div>
-            <div className={classes['song-description']}>
-                <p>Artist : {props.lyricsData.artist}</p>
-            </div>
-            <div className={classes['song-lyrics']}>
-                <div className={classes['lyrics-label']}>
-                    <p>Lyrics :</p>
+            <div className={classes['song-container']}>
+                <div className={classes['close-lyrics']} onClick={props.onHide}>X</div>
+                <div className={classes['song-title']}>
+                    <p className={classes['title']}>{props.lyricsData.title}</p>
+                    <p className={classes['title-translation']}>{props.lyricsData.titleTranslation}</p>
                 </div>
-                <div className={classes['lyrics-content']} dangerouslySetInnerHTML={{__html: props.lyricsData.lyrics}}>
+                <div className={classes['song-description']}>
+                    <p>Artist : {props.lyricsData.artist}</p>
+                </div>
+                <div className={classes['song-lyrics']}>
+                    <div className={classes['lyrics-label']}>
+                        <p>Lyrics :</p>
+                    </div>
+                    <div className={classes['lyrics-content']} dangerouslySetInnerHTML={{__html: props.lyricsData.lyrics}}>
+                    </div>
                 </div>
             </div>
         </div>
