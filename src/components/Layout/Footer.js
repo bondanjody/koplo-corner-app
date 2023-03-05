80f9dfe975040e60a5e997eea4d889aa64b9f56e
bondanjody <bondanjodys@gmail.com>
2023-03-05 22:53:10 +0700

    Menambahkan teks pemberitahuan ketika user mensubmit form subscribe

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,23 +2,35 @@ import classes from './Footer.module.css';
 import instagramIcon from '../../assets/instagram.png';
 import facebookIcon from '../../assets/facebook.png';
 import twitterIcon from '../../assets/twitter.png';
+import { Fragment, useState } from 'react';
 
 const Footer = () => {
+    const [showSubscribeSuccess, setShowSubscribeSuccess] = useState(true);
+
+    const submitHandler = () => {
+        setShowSubscribeSuccess(false);
+    }
+
     return <div className={classes.footer}>
             <div className={classes['subscribe-section']}>
                 <div className={classes['footer-header']}>
                     <p>Subscribe Newsletter</p>
                 </div>
-                <div className={classes['newsletter-description']}>
-                    <p>Get the latest news from our website !</p>
-                </div>
-                <form>
-                    <label htmlFor='name'>Your name :</label><br />
-                    <input type='text' id='name' /><br />
-                    <label htmlFor='email'>Your email :</label><br />
-                    <input type='email' id='email' /><br />
-                    <button className={classes['subscribe-button']}>Subscribe</button>
-                </form>
+                {showSubscribeSuccess && <Fragment>
+                    <div className={classes['newsletter-description']}>
+                        <p>Get the latest news from our website !</p>
+                    </div>
+                    <form onSubmit={submitHandler}>
+                        <label htmlFor='name'>Your name :</label><br />
+                        <input type='text' id='name' /><br />
+                        <label htmlFor='email'>Your email :</label><br />
+                        <input type='email' id='email' /><br />
+                        <button className={classes['subscribe-button']}>Subscribe</button>
+                    </form>
+                </Fragment>}
+                {!showSubscribeSuccess && <div className={classes['subscribe-success']}>
+                    <p>Thank you for subscribing us !</p>
+                </div>}
             </div>
             <div className={classes['social-section']}>
                 <div className={classes['footer-header']}>
